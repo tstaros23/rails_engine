d8eff70cc7a5ce6c8d52ab2bb59c1ef04cc4da2e
Ted Staros <tstaros23@gmail.com>
2021-12-07 16:01:18 -0700

    Finished API and passed test plus set up next test

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     merchants = Merchant.all
-    something = render json: MerchantSerializer.format_merchants(merchants)
+    render json: MerchantSerializer.new(merchants) #status: OK rails status codes
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,16 +7,31 @@ require 'rails_helper'
      get '/api/v1/merchants'
 
      expect(response).to be_successful
+     expect(response.status).to eq(200)
      merchants = JSON.parse(response.body, symbolize_names: true)
 
-     expect(merchants.count).to eq(3)
+     expect(merchants[:data].count).to eq(3)
 
-     merchants.each do |merchant|
+     merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
-
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant).to have_key(:type)
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to have_key(:name)
     end
+   end
+
+   it "can send one merchant" do
+     merchant = create(:merchant)
+
+     get "/api/vi/merchants/#{merchant.id}"
+
+     expect(response).to be_successful
+     expect(response.status).to eq(200)
+     merchant = JSON.parse(response.body, symbolize_names: true)
+
+     expect(merchant[:data]).to have_key(:id)
+     expect(merchant[:data]).to have_key(:type)
+     expect(merchant[:data]).to have_key(:attributes)
+     expect(merchant[:data][:attributes]).to have_key(:name)
    end
  end
