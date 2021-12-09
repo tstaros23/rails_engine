e8a569c89a3e55574159e331c1fa9b29b5fea688
Ted Staros <tstaros23@gmail.com>
2021-12-08 19:56:24 -0700

    Delete one item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def create
     created_item = Item.create(item_params)
-    something = render json: ItemSerializer.new(created_item), status: :created
+    render json: ItemSerializer.new(created_item), status: :created
   end
 
   def update
@@ -9,6 +9,11 @@ class Api::V1::ItemsController < ApplicationController
     updated_item = Item.update(item.id, item_params)
     render json: ItemSerializer.new(updated_item)
   end
+
+  def destroy
+    Item.delete(params[:id])
+  end
+
   private
     def item_params
       params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.all
-    render json: MerchantSerializer.new(merchants) #status: OK rails status codes
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    merchant = Merchant.find(params[:id])
-    render json: MerchantSerializer.new(merchant)
+    if Merchant.exists?(params[:id])
+      merchant = Merchant.find(params[:id])
+      render json: MerchantSerializer.new(merchant)
+    else
+      render json: {errors: {details: "merch doesnt exist"}}, status: 404
+    end
   end
 end
