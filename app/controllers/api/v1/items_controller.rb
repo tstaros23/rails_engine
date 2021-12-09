3ed93952298280b6c9b4c17e3a0b873429656c50
Ted Staros <tstaros23@gmail.com>
2021-12-08 18:50:50 -0700

    Create an item in endpoint

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::ItemsController < ApplicationController
+  def create
+    created_item = Item.create(item_params)
+    something = render json: ItemSerializer.new(created_item), status: :created
+  end
+
   def update
     item = Item.find(params[:id])
     updated_item = Item.update(item.id, item_params)
-    rendered = render json: ItemSerializer.new(updated_item)
+    render json: ItemSerializer.new(updated_item)
   end
   private
     def item_params
-      params.require(:item).permit(:name, :description, :unit_price)
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
     end
 end
