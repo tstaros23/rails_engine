2415846558809877b3c9c1efbc7fb03d6924014d
Ted Staros <tstaros23@gmail.com>
2021-12-09 21:46:29 -0700

    Make small change

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,8 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items, :dependent => :destroy
   belongs_to :merchant
 
-  # after_destroy :cleanup
 
   def self.search_for_names(name)
     where("name ILIKE ?", "%#{name.downcase}%")
   end
-
-  # def cleanup
-  #   boolean = Invoice.all.each do |invoice|
-  #     invoice.items.count == 1
-  #       invoice
-  #   end
-  # end
 end
